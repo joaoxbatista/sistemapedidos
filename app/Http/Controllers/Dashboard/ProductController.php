e85437ec64530a6a833fad5d0b5d9749ba4b31bb
Joao Batista Gomes Silva <jhonxbatista@gmail.com>
2017-07-22 10:00:41 -0300

    reset

--- app/Http/Controllers/Dashboard/ProductController.php
+++ app/Http/Controllers/Dashboard/ProductController.php
@@ -44,7 +44,7 @@ class ProductController extends Controller
             $name = time().$request->get('name').'.'.$extension;
             $file = $request->file('file');
 
-            Image::make($file)->resize(240, 240)->save(public_path('imgs'.$dir.'products'.$dir.$name));
+            Image::make($file)->resize(240, 240)->save(public_path('uploads'.$dir.'images'.$dir.'products'.$dir.$name));
             $request['image'] = $name;
         }
 
@@ -88,8 +88,8 @@ class ProductController extends Controller
             $extension = $request->file('file')->getClientOriginalExtension();
             $name = time().$request->get('name').'.'.$extension;
             $file = $request->file('file');
-            //Salva a imagem
-            Image::make($file)->resize(240, 240)->save(public_path('images'.$dir.'products'.$dir.$name));
+
+            Image::make($file)->resize(240, 240)->save(public_path('uploads'.$dir.'images'.$dir.'products'.$dir.$name));
             $request['image'] = $name;
         }
 
