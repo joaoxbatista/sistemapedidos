f3e79fc4c4cf6db4b1d76d8a7f53776bd4a8a990
João Batista Gomes Silva <jhonxbatista@gmail.com>
2018-03-04 08:59:23 -0300

    a

--- storage/framework/views/45c66af9c931c30d7c8b4406f9a43a8279c27707.php
+++
@@ -1,5 +0,0 @@
-<?php $__env->startSection('title'); ?> Dashboard | Home <?php $__env->stopSection(); ?>
-<?php $__env->startSection('content'); ?>
-
-<?php $__env->stopSection(); ?>
-<?php echo $__env->make('template.seller-dashboard', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
